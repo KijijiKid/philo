4d4f708266d93430b8beca7a917d8299725788fd
Mateo Andre <mandre@2-D-1.42heilbronn.de>
2025-09-21 13:56:16 +0200

    Inital Commit -> Added functioning Makefile and dir hierarchy

---
+++ Makefile
@@ -0,0 +1,60 @@
+# <==== Meta Data =====>
+NAME	:= philo
+CC		:= cc
+CFLAGS	:= -Wall -Werror -Wextra
+
+#Includes
+INC_FILES		:= inc
+
+# <==== Files & Folders =====>
+MAIN_FILE	:= main.c
+
+SRC_DIR		:= src/
+OBJS_DIR	:= objs/
+
+#Folders inside src/
+CORE_DIR	:= core/
+
+
+
+CORE_FILES	:= $(addprefix $(CORE_DIR), $(addsuffix .c, input_parsing)) # src/*.c
+
+
+# Every folder path including the .c files inside the src/ folder 
+MELTING_POT	:= $(CORE_FILES)
+
+
+# Every Folder path prefixed with src/ and the main (top level)
+SRCS		:= $(MAIN_FILE) $(addprefix $(SRC_DIR), $(MELTING_POT))
+
+# All .c to .o -> See compilation Rule $< $@
+OBJS		:= $(addprefix $(OBJS_DIR), $(SRCS:%.c=%.o))
+
+
+
+# <===== Rules =====>
+
+all: $(NAME)
+
+#Compile executable including every .o file
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -Iinclude -I$(INC_FILES) -o $(NAME)
+
+#Rule to compile all .o files
+$(OBJS_DIR)%.o: %.c | $(OBJS_DIR)
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -I $(INC_FILES) -c $< -o $@
+
+$(OBJS_DIR):
+	@mkdir -p $(OBJS_DIR)
+
+clean:
+	@rm -rf $(OBJS_DIR)
+
+fclean: clean
+	@rm -rf $(NAME)
+
+re: fclean
+	@make
+
+.PHONY: all clean fclean re
---
+++ inc/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/21 11:25:50 by mandre            #+#    #+#             */
+/*   Updated: 2025/09/21 11:31:10 by mandre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/*Common Libaries*/
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h> // get_time_of_day
+# include <pthread.h> // Thread functions
+
+#endif
