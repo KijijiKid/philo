69331dc2c43b1846e1e7858f944c4a1d33c8454b
Mateo Andre <mandre@3-C-1.42heilbronn.de>
2025-09-24 11:13:47 +0200

    V.1.1 Added assign forks and init_forks

--- src/core/forks_init.c
+++ src/core/forks_init.c
@@ -6,12 +6,23 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 10:41:09 by mandre            #+#    #+#             */
-/*   Updated: 2025/09/24 10:45:40 by mandre           ###   ########.fr       */
+/*   Updated: 2025/09/24 11:12:38 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	assign_forks(t_meta *philo_meta, philo_t *philo)
+{
+	int	curr_philo_id;
+
+	curr_philo_id = philo->id;
+	philo->r_fork = &(philo_meta->total_forks)[curr_philo_id - 1];
+	if (curr_philo_id == 0)
+		philo->l_fork = &(philo_meta->total_forks)[philo_meta->philosophers_count - 1];
+	else
+		philo->l_fork = &(philo_meta->total_forks)[curr_philo_id - 2];
+}
 
 /// @brief Creates as many forks as they are philosophers
 /// @param philo 
@@ -20,10 +31,11 @@ void	init_forks(t_meta *philo_meta)
 	int i;
 
 	i = 0;
+	philo_meta->total_forks = malloc(sizeof(pthread_mutex_t) * philo_meta->philosophers_count);
 	while (i <= philo_meta->philosophers_count)
 	{
 		if (pthread_mutex_init(&(philo_meta->total_forks)[i], NULL) != 0)
-			//Error MSG
+			printf("Error MSG has to be implemnted!!!"); // TODO
 		i++;
 	}
 }
