280e7f2872761cdd4a99e0a5219c8c84d751e357
Mateo Andre <mandre@2-D-1.42heilbronn.de>
2025-09-30 12:38:46 +0200

    Bugging

--- src/tools/output.c
+++ src/tools/output.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 15:01:19 by mandre            #+#    #+#             */
-/*   Updated: 2025/09/30 11:48:30 by mandre           ###   ########.fr       */
+/*   Updated: 2025/09/30 12:07:08 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,22 @@
 
 int	write_action(t_philo *philo, t_action action, unsigned int id)
 {
-	bool	write;
-	// size_t	time;
-
-	write = true;
 	pthread_mutex_lock(&philo->run_lock);
 	if (*philo->run_flag == false)
 		return 1;
 	pthread_mutex_unlock(&philo->run_lock);
 	pthread_mutex_lock(&philo->write_lock);
-	if (action == SLEEP && write)
+	if (action == SLEEP)
 		printf("%ld %d is sleeping\n", get_curr_time() - philo->options.start_time, id);
-	else if (action == THINK && write)
+	else if (action == THINK)
 		printf("%ld %d is thinking\n", get_curr_time() - philo->options.start_time, id);
-	else if (action == EAT && write)
+	else if (action == EAT)
 		printf("%ld %d is eating\n", get_curr_time() - philo->options.start_time, id);
-	else if (action == T_FORK && write)
+	else if (action == T_FORK)
 		printf("%ld %d has taken a fork\n", get_curr_time() - philo->options.start_time, id);
-	else if  (action == P_IS_DEAD && write)
+	else if  (action == P_IS_DEAD)
 		printf("%ld %d is dead\n", get_curr_time() - philo->options.start_time, id);
-	else if (action == P_ARE_FULL && write)
+	else if (action == P_ARE_FULL)
 		printf("Philos are full\n");
 	pthread_mutex_unlock(&philo->write_lock);
 	return (0);
