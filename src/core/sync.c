f73aaa22648f6ed976bba0e039b8db317206f3ac
Mateo Andre <mandre@2-D-1.42heilbronn.de>
2025-10-01 17:09:57 +0200

    Better commentarys for the functions

--- src/core/sync.c
+++ src/core/sync.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 16:09:24 by mandre            #+#    #+#             */
-/*   Updated: 2025/10/01 16:54:13 by mandre           ###   ########.fr       */
+/*   Updated: 2025/10/01 17:06:25 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /// @brief After all threads are created this
 /// function called by the main thread sets the 
-/// wait flag to false so the philos can begin their
+/// sync flag to true so the philos can begin their
 /// routine
 void	philo_start(t_meta *meta)
 {
@@ -24,12 +24,14 @@ void	philo_start(t_meta *meta)
 }
 
 /// @brief Holds each thread/philo in a while loop
-/// as long as the others are not created (wait_flag == true)
+/// as long as the others are not created (start == false)
+/// This resolves in a more or less sync of all threads
+/// ft_usleep(1000) => Avoid to burn 100% of the CPU so 1ms
+/// wait after each loop cycle
 void	philo_hold(t_philo *philo)
 {
 	bool	start;
 
-	
 	while (1)
 	{
 		pthread_mutex_lock(philo->sync_lock_ptr);
