5f6813a0281d0ee2aee8566e19442a84c5e7322e
KijijiKid <mateo-andre@gmx.de>
2025-10-02 17:43:00 +0200

    Meal count debug

--- src/core/monitor.c
+++ src/core/monitor.c
@@ -6,11 +6,37 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 16:55:45 by mandre            #+#    #+#             */
-/*   Updated: 2025/10/02 17:11:48 by mandre           ###   ########.fr       */
+/*   Updated: 2025/10/02 17:39:51 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static bool	check_if_fed_up(t_meta *meta)
+{
+	unsigned int	i;
+	unsigned int	meal_count;
+	bool			all_ate_enough;
+
+	// Diable eating terminator argc[5] isn't provieded
+	if (meta->options.p_mec == 0)
+		return (false);
+	i = 0;
+	all_ate_enough = true;
+	while (i < meta->options.p_num)
+	{
+		pthread_mutex_lock(&meta->philo[i].meal_count_lock);
+		meal_count = (meta->philo[i]).meal_count;
+		pthread_mutex_unlock(&meta->philo[i].meal_count_lock);
+		if ((meal_count < meta->options.p_mec))
+		{
+			all_ate_enough = false;
+			break ;
+		}
+		i++;
+	}
+	return (all_ate_enough);
+}
 
 static bool	check_dead_flag(t_meta *meta)
 {
@@ -36,9 +62,11 @@ void	init_monitor(t_meta *meta)
 	{
 		if (check_dead_flag(meta))
 			break ;
+		if (check_if_fed_up(meta))
+			break ;
 	}
 	pthread_mutex_lock(&meta->run_lock);
 	meta->run_flag =  false;
 	pthread_mutex_unlock(&meta->run_lock);
-	// write(1, "DEAD\n", 5);
+	write(1, "DEAD\n", 5);
 }
