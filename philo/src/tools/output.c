6a2d96a94978db515b03a5f033feda36db37130f
Mateo Andre <mandre@3-C-3.42heilbronn.de>
2025-10-04 14:25:12 +0200

    Performance very good but not at peak

--- philo/src/tools/output.c
+++ philo/src/tools/output.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 15:01:19 by mandre            #+#    #+#             */
-/*   Updated: 2025/10/03 18:50:58 by mandre           ###   ########.fr       */
+/*   Updated: 2025/10/04 14:21:16 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,13 @@ static void	print_status(t_philo *philo, char *str)
 
 int	write_action(t_philo *philo, t_action action, bool visible)
 {
+	bool	run;
+
+	pthread_mutex_lock(philo->run_lock_ptr);
+	run = *(philo->run_flag_ptr);
+	pthread_mutex_unlock(philo->run_lock_ptr);
+	if (!run || !philo->alive)
+		return (0);
 	if (action == SLEEP && visible)
 		print_status(philo, "is sleeping");
 	else if (action == EAT && visible)
@@ -29,6 +36,6 @@ int	write_action(t_philo *philo, t_action action, bool visible)
 	else if (action == THINK && visible)
 		print_status(philo, "is thinking");
 	else if (action == T_FORK && visible)
-		print_status(philo, "has taking fork");
+		print_status(philo, "taken fork");
 	return (0);
 }
