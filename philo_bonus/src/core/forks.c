8a723025c9f0a9ed718cf1ba88589600f18be4f9
Mateo Andre <mandre@3-B-9.42heilbronn.de>
2025-10-04 19:27:34 +0200

    Deleted total forks and assign fork function

--- philo_bonus/src/core/forks.c
+++ philo_bonus/src/core/forks.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 13:48:36 by mandre            #+#    #+#             */
-/*   Updated: 2025/10/04 19:26:35 by mandre           ###   ########.fr       */
+/*   Updated: 2025/10/04 19:27:14 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,6 @@ int	clean_total_forks(t_meta *meta)
 			return (throw_error(SEM_CLOSE_FAILED));
 		if (sem_close(&meta->philo[i].alive_lock) != 0)
 			return (throw_error(SEM_CLOSE_FAILED));
-		i++;
-	}
-	return (0);
-}
-
-/// @brief Creates the total forks/mutex array 
-/// in the amount philo's need them
-/// Assigning to l/r happens later
-int	create_total_forks(t_meta *meta)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (i < meta->options.p_num)
-	{
-		if (pthread_mutex_init(&meta->total_forks[i], NULL) != 0)
-			return (throw_error(INIT_MUTEX_FAILED));
 		i++;
 	}
 	return (0);
