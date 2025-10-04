3fb5e967f231a555d25bd9f76853b0f561a231b7
Mateo Andre <mandre@3-B-9.42heilbronn.de>
2025-10-04 19:17:46 +0200

    Meta sems initalized

--- philo_bonus/src/tools/output.c
+++ philo_bonus/src/tools/output.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 15:01:19 by mandre            #+#    #+#             */
-/*   Updated: 2025/10/04 14:21:16 by mandre           ###   ########.fr       */
+/*   Updated: 2025/10/04 19:13:39 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 static void	print_status(t_philo *philo, char *str)
 {
-	pthread_mutex_lock(philo->write_lock_ptr);
+	sem_wait(philo->write_lock_ptr);
 	printf("%ld %d %s\n", get_curr_time() - *(philo->options.start_time),
 		philo->id + 1, str);
-	pthread_mutex_unlock(philo->write_lock_ptr);
+	sem_post(philo->write_lock_ptr);
 }
 
 int	write_action(t_philo *philo, t_action action, bool visible)
 {
 	bool	run;
 
-	pthread_mutex_lock(philo->run_lock_ptr);
+	sem_wait(philo->run_lock_ptr);
 	run = *(philo->run_flag_ptr);
-	pthread_mutex_unlock(philo->run_lock_ptr);
+	sem_post(philo->run_lock_ptr);
 	if (!run || !philo->alive)
 		return (0);
 	if (action == SLEEP && visible)
