c09a9445ef173530fd87d0cce4dc042361fd9ce4
Mateo Andre <mandre@3-C-3.42heilbronn.de>
2025-10-08 17:18:29 +0200

    Still working on improving

--- philo_bonus/src/core/monitor.c
+++ philo_bonus/src/core/monitor.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 16:55:45 by mandre            #+#    #+#             */
-/*   Updated: 2025/10/08 16:59:59 by mandre           ###   ########.fr       */
+/*   Updated: 2025/10/08 17:02:00 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,20 +38,15 @@ static bool	check_if_fed_up(t_meta *meta)
 static bool	check_dead_flag(t_meta *meta)
 {
 	unsigned int	i;
-	bool			alive;
 
 	i = 0;
 	while (i < meta->options.p_num)
 	{
 		if (!is_alive(&(meta->philo[i])))
 		{
-			pthread_mutex_lock(&meta->run_lock);
 			meta->run_flag = false;
-			pthread_mutex_unlock(&meta->run_lock);
-			pthread_mutex_lock(&meta->write_lock);
 			printf("%ld %d died\n", get_curr_time()
 				- meta->start_time, meta->philo[i].id + 1);
-			pthread_mutex_unlock(&meta->write_lock);
 			return (true);
 		}
 		i++;
